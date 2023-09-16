e929dc0bef6383f64f5d5055299365b250ec9613
Muhammad Mominul Huque <mominul2082@gmail.com>
2023-09-17 05:49:44 +0600

    Daraz scrapping implemented

--- home/views.py
+++ home/views.py
@@ -1,14 +1,27 @@
+from multiprocessing import Process
 from django.shortcuts import render
 
 from .ryans import scrape_ryans
+from .daraz import scrape_daraz
 
 def home(request):
     if request.POST:
         search = request.POST["search"]
-        items = scrape_ryans(search)
+
+        ryans = scrape_ryans(search)
+        daraz = scrape_daraz(search)
 
         data = {
-            "items": items
+            "ryans": ryans,
+            "daraz": daraz,
         }
         return render(request, 'result.html', data)
     return render(request, 'home.html')
+
+
+def run_cpu_tasks_in_parallel(tasks):
+    running_tasks = [Process(target=task) for task in tasks]
+    for running_task in running_tasks:
+        running_task.start()
+    for running_task in running_tasks:
+        running_task.join()
