a359d8d50d472869445c48efcbc0209e547f482a
capt-farvez <farvez.capt@gmail.com>
2023-09-17 16:49:09 +0600

    filtering by price

--- home/views.py
+++ home/views.py
@@ -11,6 +11,11 @@ def home(request):
         ryans = scrape_ryans(search)
         daraz = scrape_daraz(search)
 
+        # Create an array containing 'ryans' and 'daraz' data
+        result_array = [ryans, daraz]
+        # Store the array in the session
+        request.session['result_array'] = result_array
+
         data = {
             "ryans": ryans,
             "daraz": daraz,
@@ -18,6 +23,19 @@ def home(request):
         return render(request, 'result.html', data)
     return render(request, 'home.html')
 
+def filter_items(request):
+    if request.POST:
+        max_price=request.POST["max_price"]
+        filtered_items = []
+        result_array= request.session['result_array']
+        for site_data in result_array:
+            site_filtered_items = []
+            if site_data['price'] <= max_price:
+                site_filtered_items.append(site_data)
+                filtered_items.append(site_filtered_items)
+    
+        return render(request, 'result.html', filtered_items)
+    return render(request, 'home.html')   
 
 def run_cpu_tasks_in_parallel(tasks):
     running_tasks = [Process(target=task) for task in tasks]
